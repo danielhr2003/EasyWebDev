44956a8eca2304466bf74c3dc58ef40dbf80f2b3
Daniel <dhrod.2003@gmail.com>
2023-02-12 06:54:36 -0500

    First JS files

---
+++ JavaScript/01 Comments, Variables, and Functions/CVF.js
@@ -0,0 +1,38 @@
+// This is a single line comment
+
+/* This is a 
+multiline comment */
+
+
+// Variables
+
+// Variables for functions or globals which are are dynamic in nature
+
+// Declare variable
+
+var myNameIs;
+
+// Initialize variable
+
+var myNameIs = "Daniel";
+
+// Variables for blocks
+
+let myNameIs = "Daniel";
+
+// Variables for blocks that cannot be changed later
+
+const myNameIs = "Daniel";
+
+// Functions 
+
+// Declare function and pass arguments
+
+function thisisaFunction(mouse) {
+    // Code to executed
+}
+
+// Call function
+
+thisisaFunction()
+
---
+++ JavaScript/03 Control Structures/controlstructure.js
@@ -0,0 +1,19 @@
+// Selection control
+
+
+// Conditional 
+
+function conditional() {
+    var name = "Juan";
+    if (name == "Daniel") {
+        console.log("You're Daniel");
+    }
+    else if (name == "Juan") {
+        console.log("You're almost Juan");
+    }
+    else {
+        console.log("You're not Daniel")
+    }
+ }
+ 
+ conditional();
